13563dfe9989c52aad3279a41a863feeffe0e904
Francis Bomher <fjbomher@Franciss-MacBook-Pro.local>
2014-03-16 15:29:13 -0400

    added a test class

---
+++ FBTest.h
@@ -0,0 +1,13 @@
+//
+//  FBTest.h
+//  GIT Test
+//
+//  Created by Francis Bomher on 3/16/14.
+//  Copyright (c) 2014 FJB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FBTest : NSObject
+
+@end
